832815682f2f4a086e4e4eab88ac8bd478ac324d
Laurent Petit <laurent.petit@gmail.com>
2011-01-23 14:47:55 +0100

    start of creation of build tools files

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject org.lpetit/net.cgrand.parsley "0.0.6.STABLE01"
+  :description "a DSL for creating parsers in Clojure"
+  :url "https://github.com/cgrand/parsley"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]])
