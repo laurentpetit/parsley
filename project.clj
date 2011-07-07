cedb683e8636e74ccd4126164784485f6d7209f8
laurentpetit <laurent.petit@gmail.com>
2011-07-07 12:09:20 +0200

    Metadata for version 0.0.9

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.lpetit/net.cgrand.parsley "0.0.6.STABLE01"
+(defproject org.lpetit/net.cgrand.parsley "0.0.9.STABLE001"
   :description "a DSL for creating parsers in Clojure"
   :url "https://github.com/cgrand/parsley"
   :dependencies [[org.clojure/clojure "1.2.0"]
