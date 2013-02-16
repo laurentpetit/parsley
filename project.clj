24ad923ffc68b48f208b966ab718c23a3191ec6c
Laurent Petit <laurent.petit@gmail.com>
2013-02-16 16:27:10 +0100

    WIP

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject org.lpetit/net.cgrand.parsley "0.0.12.STABLE001"
   :description "a DSL for creating parsers in Clojure"
   :url "https://github.com/laurentpetit/parsley"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.5.0"]])
